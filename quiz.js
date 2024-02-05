ffa6e10c5c4c15978556471596640af40ac30e49
bichhientest <bichhhiennbh@gmail.com>
2024-02-05 14:17:01 +0700

    giaidoan2

--- quiz.js
+++ quiz.js
@@ -1,4 +1,6 @@
 var app=angular.module("myapp",[]);
+
+
 app.directive("myquiz",function(myfactory){
     return {
         restrict: "AE",
@@ -20,9 +22,7 @@ app.directive("myquiz",function(myfactory){
                 scope.question=quiz.Text;
                 scope.options= quiz.Answers;
                 scope.answer=quiz.AnswerId;
-                }
-                
-                
+                }                               
             }
             scope.checkanswer= function(){
                // alert("answer")
@@ -43,6 +43,8 @@ app.directive("myquiz",function(myfactory){
         }
     }
 });
+
+
 app.factory("myfactory",function($http){
     $http.get('../db/Quizs/ADAV.js').then(function(res){
         question= res.data;
